42f000e68f3bef1f2a8f9d6b048a43c3c9d9d382
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2023-09-22 15:42:18 +0400

    Add setup.cfg, refactor user tests

---
+++ task_manager/users/tests/test_create.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse_lazy
+from .custom_test_client import CustomTestClient
+from django.contrib.messages import get_messages
+
+
+class CreateUserTest(TestCase):
+    client_class = CustomTestClient
+    url = reverse_lazy("create_user")
+
+    def test_create_user(self):
+        self.client.send_post(self.url)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_new_user_have_required_data(self):
+        response = self.client.send_post(self.url)
+        form_data = self.client.form_data
+        required_first_name = form_data['first_name']
+        required_last_name = form_data['last_name']
+        required_username = form_data['username']
+        required_password = form_data['password1']
+        new_user = User.objects.first()
+        self.assertEqual(new_user.first_name, required_first_name)
+        self.assertEqual(new_user.last_name, required_last_name)
+        self.assertEqual(new_user.username, required_username)
+        self.assertTrue(check_password(required_password, new_user.password))
+        message = (f'Success! User with username '
+                   f'{new_user.username} was created.')
+        message_presence = self.client.check_message(response, message)
+        self.assertTrue(message_presence)
+
+    def test_create_existing_user(self):
+        self.client.send_post(self.url)
+        self.client.send_post(self.url)
+        self.assertEqual(User.objects.count(), 1)
---
+++ task_manager/users/tests/test_delete.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from django.urls import reverse_lazy
+from .custom_test_client import CustomTestClient
+
+
+class DeleteUserTest(TestCase):
+    fixtures = ['users.json']
+    client = Client()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.users = User.objects
+        cls.users_in_fixture = cls.users.count()
+
+    def send_delete_request(self, pk):
+        url = reverse_lazy("delete_user", kwargs={'pk': pk})
+        return self.client.post(url)
+
+    def check_users_count(self, required_quantity):
+        self.assertEqual(self.users.count(), required_quantity)
+
+    def test_delete_user(self):
+        user = self.users.get(id=1)
+        self.client.force_login(user)
+        response = self.send_delete_request(pk=1)
+        self.check_users_count(self.users_in_fixture - 1)
+        last_user = self.users.first()
+        self.assertTrue(last_user.id == 2)
+        message = 'Success! Chosen user was deleted.'
+        message_presence = CustomTestClient.check_message(response, message)
+        self.assertTrue(message_presence)
+
+    def test_delete_another_user(self):
+        first_user = self.users.get(id=1)
+        self.client.force_login(first_user)
+        response = self.send_delete_request(pk=2)
+        self.check_users_count(self.users_in_fixture)
+        message = 'Access granted only for authorized owner.'
+        message_presence = CustomTestClient.check_message(response, message)
+        self.assertTrue(message_presence)
+
+    def test_delete_user_without_authorize(self):
+        self.send_delete_request(pk=1)
+        self.check_users_count(self.users_in_fixture)
